6e5ef450b0c5227dd61850c159efd49434bdde54
Andre Fernandes <dedeh159@gmail.com>
2024-10-04 09:19:02 -0300

    inserindo dados pelo java junto com a consulta

--- src/main/java/org/senai/Main.java
+++ src/main/java/org/senai/Main.java
@@ -4,15 +4,40 @@ import org.senai.infra.MySQLConnection;
 
 import javax.swing.*;
 import java.sql.*;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
        // MySQLConnection connector = new MySQLConnection();
 
         //outro exemplo
         Connection conexao = MySQLConnection.conexao();
+        System.out.println("Insita o nome do usuário: ");
+        String name = sc.nextLine();
+
+        System.out.println("Insira o e-mail do usuário");
+        String e_mail = sc.nextLine();
+
+        String insertCommand = "INSERT INTO usuario (nome, email) VALUES (?, ?)";
+
+        try {
+            PreparedStatement stmt = conexao.prepareStatement(insertCommand);
+            stmt.setString(1, name);
+            stmt.setString(2, e_mail);
+
+            int rowInserted = stmt.executeUpdate();
+
+            if (rowInserted > 0) {
+                System.out.println("Dados inseridos com sucesso!");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
         String comando = "SELECT * FROM usuario";
 
@@ -32,6 +57,7 @@ public class Main {
                 System.out.println("Oi " + nome + " seu email é " + email + "?");
 
             }
+            conexao.close();
         }
         catch (Exception e) {
             throw new RuntimeException(e);
